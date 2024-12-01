0a50d7e591922bc4f8d7a110e5f9e06e88931e49
mohrly123 <168989632+mohrly123@users.noreply.github.com>
2024-12-01 09:35:47 +0100

    Update verschiebungsrechner.jsx

--- src/Verschiebungsrechner/verschiebungsrechner.jsx
+++ src/Verschiebungsrechner/verschiebungsrechner.jsx
@@ -1,16 +1,44 @@
 import styles from "./verschiebungsrechner.module.css"
+import { useState } from "react";
 
 export default function Verschiebungsrechner(){
+    // Wenn das IST kleiner als das Soll dann LINKS Schieben
+    // Wenn das IST größer als das Soll dann RECHTS Schieben
 
-    // Soll = 150 mm
-    // Ist = 180 mm
-    // 
+    const [sollWert, setSollWert] = useState("");
+    const [istWert, setIstWert] = useState("");
+
+    function handleSollWert(event){
+        setSollWert(event.target.value);
+    }
+    function handleIstWert(event){
+        setIstWert(event.target.value);
+    }
+    const programm = () => {
+        console.log(`Eingabe SOLL: ${sollWert}`);
+        console.log(`Eingabe IST: ${istWert}`);
+    }
+
 
 
     return(
         <div className={styles.hauptcontainer}>
             <div className={styles.card}>
                 <p>Verschiebungsrechner</p>
+                <input
+                    onChange={handleSollWert}
+                    type="number"
+                    placeholder="SOLL Wert in mm"
+                />
+                <input
+                    onChange={handleIstWert} 
+                    type="number"
+                    placeholder="IST Wert in mm"
+                />
+                <button
+                    type="button"
+                    onClick={programm}
+                >Berechnen</button>
             </div>
         </div>
     );
